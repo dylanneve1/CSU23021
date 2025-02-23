f188340e9310627223f1cd4c1792f8346474088e
Dylan Neve <dylanneve1@gmail.com>
2025-02-23 21:52:11 +0000

    pico-apps: assignments: move assign01 logging into C section

--- assignments/assign01/assign01.c
+++ assignments/assign01/assign01.c
@@ -2,8 +2,63 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
+// Define log message mappings
+#define setup_ebutton_msg    "button_setup:          setting up EN button on pin 21\n"
+#define setup_ubutton_msg    "button_setup:          setting up UP button on pin 22\n"
+#define setup_dbutton_msg    "button_setup:          setting up DOWN button on pin 20\n"
+#define event_msg            "gpio_isr:              an event has occurred!\n"
+#define event_eb_msg         "enable_button_event:   enable button has been pressed\n"
+#define event_ub_msg         "up_button_event:       up button has been pressed\n"
+#define event_db_msg         "down_button_event:     down button has been pressed\n"
+#define setup_led_msg        "led_setup:             setting up LED on pin 25\n"
+#define led_on_msg           "led_set_on:            toggling LED state to on\n"
+#define led_off_msg          "led_set_off:           toggling LED state to off\n"
+#define reset_timer_msg      "reset_timer_event:     timer reset to DFLT_ALARM_TIME\n"
+
 // Declare the main assembly code entry point.
 void main_asm();
+
+// Logger function - called from assembly
+void log_message(int code) {
+    switch (code) {
+        case 1:
+            printf("%s", setup_ebutton_msg);
+            break;
+        case 2:
+            printf("%s", setup_ubutton_msg);
+            break;
+        case 3:
+            printf("%s", setup_dbutton_msg);
+            break;
+        case 4:
+            printf("%s", event_msg);
+            break;
+        case 5:
+            printf("%s", event_eb_msg);
+            break;
+        case 6:
+            printf("%s", event_ub_msg);
+            break;
+        case 7:
+            printf("%s", event_db_msg);
+            break;
+        case 8:
+            printf("%s", setup_led_msg);
+            break;
+        case 9:
+            printf("%s", led_on_msg);
+            break;
+        case 10:
+            printf("%s", led_off_msg);
+            break;
+        case 11:
+            printf("%s", reset_timer_msg);
+            break;
+        default:
+            printf("Unknown log event: %d\n", code);
+            break;
+    }
+}
 
 // Initialise a GPIO pin – see SDK for detail on gpio_init()
 void asm_gpio_init(uint pin) {
